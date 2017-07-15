99726b8509a9057be9211dc42c2179e432c59f2f
Oliver Pieloth <oliver.pieloth@gmx.de>
2017-07-15 09:00:55 +0200

    A4E46 Setup Track Throttles

--- BattleTank/Source/BattleTank/Public/TankTurret.h
+++ BattleTank/Source/BattleTank/Public/TankTurret.h
@@ -6,7 +6,7 @@
 #include "TankTurret.generated.h"
 
 /**
- * 
+ *  TankTurret is used to set the max turning speed of the turret, and to rotate the turret
  */
 UCLASS(meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankTurret : public UStaticMeshComponent
