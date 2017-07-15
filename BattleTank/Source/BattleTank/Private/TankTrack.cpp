ce39152bdeabfc1f0a42c639a4dc761ea0df93f0
Oliver Pieloth <oliver.pieloth@gmx.de>
2017-07-15 13:00:16 +0200

    A4E48 Physics Materials & Friction

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -13,7 +13,7 @@ void UTankTrack::SetThrottle(float Throttle)
     auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
     auto ForceLocation = GetComponentLocation();
     auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
-    TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+    TankRoot->AddImpulseAtLocation(ForceApplied, ForceLocation);
 }
 
 
