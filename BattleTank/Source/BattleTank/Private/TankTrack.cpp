549c538856e1ad9c93939f430cc28ade77c1c107
Oliver Pieloth <oliver.pieloth@gmx.de>
2017-07-15 14:49:33 +0200

    A4E49 Fly-By-Wire Control System

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -13,7 +13,8 @@ void UTankTrack::SetThrottle(float Throttle)
     auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
     auto ForceLocation = GetComponentLocation();
     auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
-    TankRoot->AddImpulseAtLocation(ForceApplied, ForceLocation);
+    // AddForceAtLocation would not apply an accelaration
+    TankRoot->AddImpulseAtLocation(ForceApplied, ForceLocation); // TODO add max speed
 }
 
 
